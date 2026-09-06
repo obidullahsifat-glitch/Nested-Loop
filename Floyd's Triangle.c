62459af8b4a270322ab7d4fad2ad78e7ca37b12e
obidullahsifat-glitch <obidullahsifat29210@gmail.com>
2026-09-06 15:12:58 +0600

    Add files via upload

---
+++ Floyd's Triangle.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,n;
+    int count=1;
+    printf("Enter The Value");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+
+        for(j=1;j<=i;j++)
+        {
+            printf("%d ",count);
+            count++;
+        }
+        printf("\n");
+    }
+
+
+
+
+
+
+
+
+}
+
