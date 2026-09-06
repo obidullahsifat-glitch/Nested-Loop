0c23b110ddeecef0f2573860b54045f687048d3d
obidullahsifat-glitch <obidullahsifat29210@gmail.com>
2026-09-06 14:38:54 +0600

    Add files via upload

---
+++ Inverted Triangle Pattern.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int i,j,n;
+    printf("Enter The Value");
+    scanf("%d",&n);
+    for(i=n;i>=1;i--)
+    {
+
+        for(j=i;j>=1;j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+
+
+
+
+
+
+
+}
